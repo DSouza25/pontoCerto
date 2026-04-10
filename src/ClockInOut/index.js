f8ddb2235e2b7b93d351d8b18d44afc84d762a48
Dário Souza <dariodev25@gmail.com>
2026-04-10 18:29:27 -0400

    funcionamento do nav errado

--- src/ClockInOut/index.js
+++ src/ClockInOut/index.js
@@ -1,16 +1,26 @@
 import { useEffect, useState } from 'react';
 import './clockInOut.css';
 import fingerprint from '../assets/fingerprint.png';
+import { NavLink } from 'react-router-dom';
 
 
 export default function ClockInOut(){
 
     return(
         <div className='pageContainer'>
+            <header className='menuRegister'>
+                <p>Ponto Certo</p>
+                <nav>
+                    <NavLink to="/clockInOut" className={({isActive}) => isActive ? "active": ""} end>Início</NavLink>
+                    <NavLink to="/clockInOut/" className={({isActive}) => isActive ? "active": ""} end>Histórico</NavLink>
+                </nav>
+                <a>Sair</a>
+            </header>
             <div className='checkContainer'>
-                <h1>Hora atual</h1>
-                <p>data atual</p>
-                <button>Registrar Ponto</button>
+                <div className='aBox'>Bloco A</div>
+                <div className='bBox'>Bloco B</div>
+                <div className='cBox'>Bloco C</div>
+                <div className='dBox'>Bloco D</div>
             </div>
         </div>
     );
