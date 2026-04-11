1a132bfee8a795430e25fd181263bb851967895f
Dário Souza <dariodev25@gmail.com>
2026-04-11 00:05:24 -0400

    movimento de ancoragem funcionando

--- src/ClockInOut/index.js
+++ src/ClockInOut/index.js
@@ -1,27 +1,36 @@
 import { useEffect, useState } from 'react';
 import './clockInOut.css';
 import fingerprint from '../assets/fingerprint.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 
 export default function ClockInOut(){
+
+    const location = useLocation();
+    console.log(location);
+    
 
     return(
         <div className='pageContainer'>
             <header className='menuRegister'>
                 <p>Ponto Certo</p>
                 <nav>
-                    <NavLink to="/clockInOut" className={({isActive}) => isActive ? "active": ""} end>Início</NavLink>
-                    <NavLink to="/clockInOut/" className={({isActive}) => isActive ? "active": ""} end>Histórico</NavLink>
+                    <a href='#check' className={ location.hash === "#check" ? "active": ""} end>Início</a>
+                    <a href='#registers' className={ location.hash === "#registers" ? "active": ""} end>Histórico</a>
                 </nav>
                 <a>Sair</a>
             </header>
-            <div className='checkContainer'>
-                <div className='aBox'>Bloco A</div>
-                <div className='bBox'>Bloco B</div>
-                <div className='cBox'>Bloco C</div>
-                <div className='dBox'>Bloco D</div>
-            </div>
+            <main className='mainBlock'>
+                <div id='check' className='checkContainer'>
+                    <div className='aBox'>Bloco A</div>
+                    <div className='bBox'>Bloco B</div>
+                    <div className='cBox'>Bloco C</div>
+                    <div className='dBox'>Bloco D</div>
+                </div>
+                <div id='registers' className='registersContainer'>
+
+                </div>
+            </main>
         </div>
     );
 }
