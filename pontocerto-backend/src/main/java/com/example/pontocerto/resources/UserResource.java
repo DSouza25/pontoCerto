f8596f34c1cb28618dc1b782690dc6612941eb2a
Dário Souza <dariodev25@gmail.com>
2026-04-23 23:43:12 -0400

    testando método post para fazer a autenticação de login com o spring security posteriormente

--- pontocerto-backend/src/main/java/com/example/pontocerto/resources/UserResource.java
+++ pontocerto-backend/src/main/java/com/example/pontocerto/resources/UserResource.java
@@ -1,11 +1,9 @@
 package com.example.pontocerto.resources;
 
+import com.example.pontocerto.domain.User;
 import com.example.pontocerto.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -18,5 +16,10 @@ public class UserResource {
     @GetMapping
     public String teste(){
         return userService.teste();
+    }
+
+    @PostMapping
+    public void pegarDados(@RequestBody User obj){
+        System.out.println(obj.getName());
     }
 }
